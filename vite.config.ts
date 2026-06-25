899dfec5cc80ff77a915586a1a635019d2498559
khareem23 <kareem.olayinka@outlook.com>
2026-06-25 21:31:22 +0100

    Initial portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/olayinka_kareem/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
